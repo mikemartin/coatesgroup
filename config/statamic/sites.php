68f5c5ac31dbdf56393c81ff1d72558135f4fbb1
Mike Martin <hello@mike-martin.ca>
2024-04-08 11:27:39 +1000

    Add multisite

--- config/statamic/sites.php
+++ config/statamic/sites.php
@@ -16,10 +16,59 @@ return [
     'sites' => [
 
         'en' => [
-            'name' => config('app.name'),
+            'name' => 'English',
             'locale' => 'en_US',
             'url' => '/'
         ],
 
+        'fr' => [
+          'name' => 'Français',
+          'locale' => 'fr_FR',
+          'lang' => 'fr',
+          'url' => '/fr/',
+        ],
+
+        'ja' => [
+            'name' => '日本語',
+            'locale' => 'ja_JP',
+            'lang' => 'ja',
+            'url' => '/ja/',
+        ],
+
+        'zh_cn' => [
+            'name' => '中文',
+            'locale' => 'zh_CN',
+            'lang' => 'zh_CN',
+            'url' => '/zh_cn/',
+        ],
+
+        'zh_tw' => [
+          'name' => '中文',
+          'locale' => 'zh_TW',
+          'lang' => 'zh_TW',
+          'url' => '/zh_tw/',
+        ],
+
+        'ar' => [
+          'name' => 'العربية',
+          'locale' => 'ar_SA',
+          'lang' => 'ar',
+          'url' => '/ar/',
+        ],
+
+        'hi' => [
+          'name' => 'हिन्दी',
+          'locale' => 'hi_IN',
+          'lang' => 'hi',
+          'url' => '/hi/',
+        ],
+
+        'nl' => [
+            'name' => 'Nederlands',
+            'locale' => 'nl_NL',
+            'lang' => 'nl',
+            'url' => '/nl/',
+        ],
+        
     ],
 ];
